3830f86ca35553afe0175325c7f849f8884f4511
Anubhav Tiwari <anubhavtiwaricp@gmail.com>
2022-01-23 00:33:18 +0530

    Register User Functionality

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutPage, name='logout'),
+    path('register/', views.registerPage, name='register'),
 ]
--- userprofile/views.py
+++ userprofile/views.py
@@ -2,6 +2,7 @@ from operator import methodcaller
 from django.shortcuts import redirect, render
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
+from .forms import customUserCreationForm
 
 
 def loginPage(request):
@@ -32,3 +33,22 @@ def logoutPage(request):
         logout(request)
 
         return redirect('login')
+
+
+def registerPage(request):
+    form = customUserCreationForm()
+    
+    if request.method == 'POST':
+        form = customUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            login(request, user)
+            return redirect('home')
+
+
+
+    context = {'form': form}
+    return render(request, 'userprofile/register.html', context)
